8d3d8539bbcfc3dad243e7d35cbe9120ec621b48
Mime Jr <petravidovic8@gmail.com>
2022-09-12 23:36:40 -0400

    edit html heading and add JS

---
+++ java.js
@@ -0,0 +1,18 @@
+const question = document.querySelector('#question');
+
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+
+const progressText = document.querySelector('#progressText');
+
+const scoreText = document.querySelector('#score');
+
+let currentQuestion = ();
+let acceptingAnswers = true;
+let score = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 
+    }
+]
